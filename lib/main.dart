0c6b7c8419b5157d00104e6f7d3996cca13089d0
Anatolyi <just.wt19i@gmail.com>
2022-07-09 19:06:38 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:my_warehouse/screens/screen0.dart';
+
+void main() {
+  runApp(const MainScreen());
+}
---
+++ lib/widgets/drawer.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:my_warehouse/screens/screen1.dart';
+
+class MainDrawer extends StatelessWidget {
+  const MainDrawer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          /*const UserAccountsDrawerHeader(
+            accountName: Text('Адміністратор'),
+            accountEmail: Text('admin@gmail.com'),
+            currentAccountPicture: CircleAvatar(
+              backgroundImage: AssetImage('/assets/images/drawer/logo.png'),
+            ),
+          ),*/
+
+          Padding(
+            padding: const EdgeInsets.only(top: 20.0),
+            child: ElevatedButton(
+                child: const Text("Налаштування підключення", style: TextStyle(fontSize: 20), textAlign: TextAlign.center),
+                onPressed: () {
+                  Navigator.pushNamed(context, 'settings');
+                }),
+          ),
+          ElevatedButton(
+              child: const Text("Нормативно-довідкова інформація", style: TextStyle(fontSize: 20), textAlign: TextAlign.center),
+              onPressed: () {
+                Navigator.pushNamed(context, 'settings');
+              }),
+          // Column(
+          //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          //   children: [
+          //     Text('Hello'),
+          //   ],
+          // )
+        ],
+      ),
+    );
+  }
+}
