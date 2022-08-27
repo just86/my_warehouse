78587148a4e66b27114d248fc78da33277e893a3
Anatolyi <just.wt19i@gmail.com>
2022-08-27 18:46:23 +0300

    save 27082022

--- lib/screens/documents/inventory/inventory_data.dart
+++ lib/screens/documents/inventory/inventory_data.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:my_warehouse/widgets/db/sql_helper.dart';
+//import 'package:my_warehouse/widgets/db/sql_helper.dart';
 
 class InventoryData extends StatefulWidget {
   const InventoryData({Key? key}) : super(key: key);
@@ -11,20 +11,81 @@ class InventoryData extends StatefulWidget {
 class _InventoryDataState extends State<InventoryData> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child:
-        Scaffold(
-          appBar: AppBar(
-            title: Text('Дані інвентаризації')),
-            body: Column(children:[ Text('Hi'),
-              Container(
-                height: 10,
-                color: Colors.cyan,
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(title: const Text('Дані інвентаризації')),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(children: [
+                Expanded(
+                  child: Align(
+                    alignment: Alignment.topLeft,
+                    child: Container(
+                      color: Colors.white,
+                      child: const Text(
+                        'DocName',
+                        style: TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Align(
+                    alignment: Alignment.topRight,
+                    child: Container(
+                      color: Colors.white,
+                      child: const Text(
+                        'DateTime',
+                        style: TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ),
+                // Expanded(
+                //   child: (
+                //       Column(
+                //          crossAxisAlignment: CrossAxisAlignment.start,
+                //          children: [
+                //            // Row(children: [
+                //            //   TextField(decoration: InputDecoration(hintText: 'Назва:')),
+                //            //   Text('Дата створення')
+                //            // ],)
+              ]),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: const [
+                  Expanded(
+                      child: Align(
+                    alignment: Alignment.topRight,
+                    child: Text('Кількість записів: 10000'),
+                  ))
+                ],
               ),
-              Text('Hello')
-            ]
+            ),
+            Expanded(child: 
+              ListView(
+                children: const [
+                  Text('data'),
+                  Text('data'),
+                  Text('data'),
+                  Text('data'),
+                  Text('data'),
+                ],
+              )
+            ),
 
-          ),
-        )
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () {},
+        ),),
     );
   }
 }
